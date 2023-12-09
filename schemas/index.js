82930c1a9def1749ebe38bd1a9ec7d91cd024eaf
ogorei <reinasuzuki853@gmail.com>
2023-12-09 15:08:09 +0900

    added posts

--- schemas/index.js
+++ schemas/index.js
@@ -1,5 +1,6 @@
 import banner from "./banner";
 import product from "./product";
 import wineries from "./wineries";
+import post from "./post";
 
-export const schemaTypes = [product, banner, wineries]
+export const schemaTypes = [product, banner, wineries, post]
